99cacc2ffa37910d86cd7dd143a5cbd5351a1575
h-tsubo <saikyousan15@gmail.com>
2025-04-20 11:39:57 +0900

    viteの動作確認とdebugbarの導入

--- app/resources/views/top.blade.php
+++ app/resources/views/top.blade.php
@@ -1,3 +1,12 @@
-<div>
-    <!-- Smile, breathe, and go slowly. - Thich Nhat Hanh -->
-</div>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>テストページ</title>
+    @vite('resources/js/app.js')
+</head>
+<body>
+    <h1>テストページ</h1>
+    <p>Viteと連携できているか確認中！</p>
+</body>
+</html>
